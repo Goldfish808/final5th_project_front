88a85297161729edd8214872725edb839f1d585a
Goldfish808 <tjddns0853@gmail.com>
2022-12-01 20:11:06 +0900

    [ SW ] home TOP 부분

--- lib/view/pages/main/home/widget/home_page_top_widget.dart
+++ lib/view/pages/main/home/widget/home_page_top_widget.dart
@@ -2,36 +2,106 @@ import 'package:flutter/material.dart';
 import 'package:riverpod_firestore_steam1/core/theme.dart';
 
 class HomePageTopWidget extends StatelessWidget {
-  const HomePageTopWidget({Key? key}) : super(key: key);
+  HomePageTopWidget({Key? key}) : super(key: key);
+
+  final List<String> week = ["월", "화", "수", "목", "금", "토", "일"];
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         _buildTopfirstline(),
-        Container(
-          padding: EdgeInsets.only(left: 20),
-          margin: EdgeInsets.symmetric(vertical: 20.0),
+        SizedBox(height: 18),
+        _buildDay(),
+        SizedBox(height: 16),
+        _builSchedule(),
+      ],
+    );
+  }
+
+  Widget _builSchedule() {
+    return Container(
+      padding: const EdgeInsets.only(left: 20),
+      //margin: const EdgeInsets.symmetric(vertical: 20.0),
+      height: 170.0,
+      child: ListView.builder(
+        itemCount: week.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => Container(
+          padding: EdgeInsets.only(right: 12),
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              //padding: EdgeInsets.only(right: 12),
+              alignment: Alignment.center,
+              width: 130,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 3),
+                    child: Text(week[index], style: textTheme(color: kchacholGreyColor()).bodyText1),
+                  ),
+                  Text("2" + "$index", style: textTheme().bodyText1),
+                ],
+              ),
+              decoration: BoxDecoration(
+                color: klightGreyColor(),
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDay() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20),
+      child: Container(
+        decoration: BoxDecoration(
+          color: klightGreyColor(),
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              bottomLeft: Radius.circular(10),
+              bottomRight: Radius.zero,
+              topRight: Radius.zero),
+        ),
+        padding: const EdgeInsets.all(4),
+        child: Container(
+          padding: const EdgeInsets.only(left: 4),
+          //margin: const EdgeInsets.symmetric(vertical: 20.0),
           height: 54.0,
           child: ListView.builder(
-            itemCount: 8,
+            itemCount: week.length,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) => Container(
               padding: EdgeInsets.only(right: 12),
-              child: Container(
-                //padding: EdgeInsets.only(right: 12),
-                alignment: Alignment.center,
-                width: 42,
-                child: Icon(Icons.calendar_today_outlined, size: 15),
-                decoration: BoxDecoration(
-                  color: klightGreyColor(),
-                  borderRadius: BorderRadius.circular(6),
+              child: GestureDetector(
+                onTap: () {},
+                child: Container(
+                  //padding: EdgeInsets.only(right: 12),
+                  alignment: Alignment.center,
+                  width: 42,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 10, bottom: 3),
+                        child: Text(week[index], style: textTheme(color: kchacholGreyColor()).bodyText1),
+                      ),
+                      Text("2" + "$index", style: textTheme().bodyText1),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color(0xffFFFFFF),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
             ),
           ),
-        )
-      ],
+        ),
+      ),
     );
   }
 
