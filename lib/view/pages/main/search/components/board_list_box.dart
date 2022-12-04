970bb9b7a5f7e2d856a74b1a78a5ff8f80738f3e
Lee-na-yun <happyin3333@nate.com>
2022-12-05 00:08:32 +0900

    [ny] 게시글 리스트 슬라이드 이미지 효과 적용 완료

--- lib/view/pages/main/search/components/board_list_box.dart
+++ lib/view/pages/main/search/components/board_list_box.dart
@@ -1,21 +1,16 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:favorite_button/favorite_button.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:riverpod_firestore_steam1/core/theme.dart';
 import 'package:riverpod_firestore_steam1/models/board.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/search/board_list_comment_page.dart';
-
-List<String> images = [
-  "assets/board_1.png",
-  "assets/board_2.png",
-  "assets/board_3.png",
-];
+import 'package:riverpod_firestore_steam1/view/pages/main/search/components/slide_image.dart';
 
 class BoardListBox extends StatelessWidget {
   BoardListBox({Key? key, required this.board}) : super(key: key);
   final Board board;
-  //final CarouselController _controller = CarouselController();
-  final _pageController = PageController(viewportFraction: 0.8, initialPage: 1);
+
   int? activePage = 1;
 
   @override
@@ -23,27 +18,13 @@ class BoardListBox extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(left: 20, right: 20, top: 24),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           _buildProfile(),
+          SizedBox(height: 8),
           Column(
             children: [
-              PageView.builder(
-                itemCount: 2,
-                pageSnapping: true,
-                controller: _pageController,
-                itemBuilder: (context, pagePosition) {
-                  return Container(
-                    margin: EdgeInsets.all(10),
-                    child: Image.asset(
-                      images[pagePosition],
-                    ),
-                  );
-                },
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: indicators(images.length, activePage),
-              ),
+              SlideImage(),
             ],
           ),
           _buildFavoriteAndCommIcon(context),
@@ -51,17 +32,6 @@ class BoardListBox extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  List<Widget> indicators(imagesLength, currentIndex) {
-    return List<Widget>.generate(imagesLength, (index) {
-      return Container(
-        margin: EdgeInsets.all(3),
-        width: 10,
-        height: 10,
-        decoration: BoxDecoration(color: currentIndex == index ? Colors.black : Colors.black26, shape: BoxShape.circle),
-      );
-    });
   }
 
   Row _buildProfile() {
@@ -72,34 +42,41 @@ class BoardListBox extends StatelessWidget {
           height: 30,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.all(
-              Radius.circular(8),
+              Radius.circular(10),
             ),
-            image: DecorationImage(image: AssetImage(board.userImg), fit: BoxFit.cover),
+            image: DecorationImage(
+                image: AssetImage(board.userImg), fit: BoxFit.cover),
           ),
         ),
         SizedBox(width: 10),
         Text(
           board.name,
-          style: textTheme(color: kPrimaryColor(), weight: FontWeight.bold).headline3,
+          style: textTheme(color: kPrimaryColor(), weight: FontWeight.bold)
+              .headline3,
         ),
       ],
     );
   }
 
-  Column _buildText() {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          board.title,
-          style: textTheme(color: kPrimaryColor(), weight: FontWeight.bold).bodyText1,
-        ),
-        SizedBox(height: 4),
-        Text(
-          board.content,
-          style: textTheme(color: kchacholGreyColor(), weight: FontWeight.w200).bodyText1,
-        )
-      ],
+  Widget _buildText() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            board.title,
+            style: textTheme(color: kPrimaryColor(), weight: FontWeight.bold)
+                .bodyText1,
+          ),
+          SizedBox(height: 2),
+          Text(
+            board.content,
+            style: textTheme(color: kPrimaryColor(), weight: FontWeight.w200)
+                .bodyText1,
+          )
+        ],
+      ),
     );
   }
 
@@ -119,19 +96,22 @@ class BoardListBox extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 14),
+          padding: EdgeInsets.zero,
           child: Container(
-            width: 15,
-            height: 15,
+            //width: 15,
+            height: 24,
             child: IconButton(
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => BoardListCommentPage()),
+                  MaterialPageRoute(
+                      builder: (context) => BoardListCommentPage()),
                 );
               },
-              padding: EdgeInsets.zero,
-              icon: Image.asset("assets/comment_icon.png"),
+              padding: EdgeInsets.only(top: 6),
+              icon:
+                  Image.asset("assets/icon_comment.png", width: 15, height: 15),
+              //Icon(FontAwesomeIcons.comment)),
             ),
           ),
         ),
