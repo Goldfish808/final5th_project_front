8f2158750255181e24be9dba29b6aff9d6d9fe56
Goldfish808 <tjddns0853@gmail.com>
2022-12-05 14:04:49 +0900

    [ SW ] List Slidable

--- lib/view/pages/main/home/my_home_page.dart
+++ lib/view/pages/main/home/my_home_page.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:riverpod_firestore_steam1/core/theme.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/components/home_app_bar.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/home/home_page_top.dart';
 
+import '../../../../models/todo.dart';
+
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
+  final List<ToDo> items = List.of(ToDoList);
 
   @override
   Widget build(BuildContext context) {
@@ -51,41 +55,66 @@ class MyHomePage extends StatelessWidget {
   }
 
   Widget _TimelineList() {
-    return Container(
-      padding: EdgeInsets.only(left: 20, right: 20, bottom: 4),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: Container(
-              height: 48,
-              padding: EdgeInsets.symmetric(horizontal: 18),
-              decoration: BoxDecoration(
-                color: klightGreyColor(),
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-              ),
-              child: Row(
-                children: [
-                  Container(
-                    width: 6,
-                    height: 6,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(6)),
-                      color: kchacholGreyColor(),
-                    ),
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    "플러터 디자인 하기",
-                    style: textTheme(color: kPrimaryColor(), weight: FontWeight.w500).headline3,
-                  ),
-                ],
-              ),
-            ),
+    return Slidable(
+      key: Key(items[1].content),
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        dismissible: DismissiblePane(onDismissed: () {}),
+        children: const [
+          SlidableAction(
+            onPressed: printSome,
+            //backgroundColor: Color(0xFF7BC043),
+            //foregroundColor: kchacholGreyColor(),
+            icon: CupertinoIcons.gear_big,
+          ),
+          SlidableAction(
+            onPressed: printSome,
+            //backgroundColor: Color(0xFF0392CF),
+            foregroundColor: primary,
+            icon: CupertinoIcons.trash,
           ),
         ],
       ),
+      child: Container(
+        padding: EdgeInsets.only(left: 20, right: 20, bottom: 4),
+        child: Row(
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Container(
+                height: 48,
+                padding: EdgeInsets.symmetric(horizontal: 18),
+                decoration: BoxDecoration(
+                  color: klightGreyColor(),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 6,
+                      height: 6,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(6)),
+                        color: kchacholGreyColor(),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      "플러터 디자인 하기",
+                      style: textTheme(color: kPrimaryColor(), weight: FontWeight.w500).headline3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+void printSome(BuildContext context) {
+  print("클릭됨");
 }
