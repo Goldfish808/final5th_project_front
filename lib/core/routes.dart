5ce0cba9f2b84199871dd64068f8808ac99cef0f
unknown <Goldfish808>
2022-12-11 16:26:25 +0900

    [ SW ] Response DTO

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -2,5 +2,6 @@ class Routers {
   static String home = "/home";
   static String login = "/login";
   static String findPassword = "/findPassword";
+  //수정필요 API 컨트롤러 문서 보고 작성할 것
   static String join = "/join";
 }
---
+++ lib/util/response_util.dart
@@ -0,0 +1,13 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+import '../dto/response_dto.dart';
+
+ResponseDto toResponseDto(Response response) {
+  Map<String, dynamic> responseMap = jsonDecode(response.body); // 문자열 -> Map
+
+  ResponseDto responseDto =
+      ResponseDto.fromJson(responseMap); // Map -> Dart Class
+  return responseDto;
+}
