71a8d5ffac9e8a20f94d2b08f4d8f32d91fdfa4c
Goldfish808 <tjddns0853@gmail.com>
2022-12-13 21:50:55 +0900

    [ SW ] slghswls 채팅 테스트 중... ?

--- lib/core/util/constant/move.dart
+++ lib/core/util/constant/move.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/chat/chat_page.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/chat/chat_room_page.dart';
+import 'package:riverpod_firestore_steam1/view/pages/main/chat/chat_test_page.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/home/friend_home_page.dart';
 import 'package:riverpod_firestore_steam1/view/pages/main/home/notice_page.dart';
 
@@ -22,6 +23,7 @@ class Move {
   static String writePage = "/write";
   static String chatPage = "/chat";
   static String noticePage = "/notice";
+  static String chatRoomPageTest = "/roomtest";
 }
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
@@ -36,5 +38,6 @@ Map<String, Widget Function(BuildContext)> getRouters() {
     Move.friendPage: (context) => FriendHomePage(),
     Move.chatPage: (context) => const ChatRoomPage(),
     Move.noticePage: (context) => NoticePage(),
+    Move.chatRoomPageTest: (context) => RoomListPageTest(),
   };
 }
